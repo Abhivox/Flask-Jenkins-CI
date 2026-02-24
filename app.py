cd3ef6946f3d52ecd7247f3496c1f84132da8c0e
Abhivox <136894993+Abhivox@users.noreply.github.com>
2026-02-24 12:08:53 +0530

    Fix indentation for main check in app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask( __name__)
 @app.route("/")
 def home():
     return "hello from Flask + Jenkins"
-if __name__ == "__main__"
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
