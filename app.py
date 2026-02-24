5ea0f7764f179d08eb10717c4b7c56396cb5889d
Abhivox <abishekcabishekc@example.com>
2026-02-24 11:10:38 +0530

    Dockerfile added

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+<<<<<<< HEAD
 app = Flask( __name__)
+=======
+app = Flask(__name__)
+>>>>>>> dfbcf93 (Dockerfile added)
 @app.route("/")
 def home():
     return "hello from Flask + Jenkins"
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     app.run(debug=True)
+=======
+    app.run(host="0.0.0.0", port=5000)
+>>>>>>> dfbcf93 (Dockerfile added)
