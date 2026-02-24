44e5286fcd16e07677db5b1a49f08a9ece2b937a
Abhivox <136894993+Abhivox@users.noreply.github.com>
2026-02-24 11:29:18 +0530

    Fix Flask app initialization and run configuration

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 from flask import Flask
-<<<<<<< HEAD
 app = Flask( __name__)
-=======
-app = Flask(__name__)
->>>>>>> dfbcf93 (Dockerfile added)
 @app.route("/")
 def home():
     return "hello from Flask + Jenkins"
-
-if __name__ == "__main__":
-<<<<<<< HEAD
-    app.run(debug=True)
-=======
+if __name__ == "__main__"
     app.run(host="0.0.0.0", port=5000)
->>>>>>> dfbcf93 (Dockerfile added)
+
